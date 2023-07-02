e9043bdd844610410fcbdc644cadcb428d874635
ligia salazar <ligiasalazar@MacBook-Air-de-ligia.local>
2023-07-02 08:01:38 -0700

    nuevo repositorio

---
+++ MedicApp/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicApp
+//
+//  Created by ligia salazar on 7/2/23.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
